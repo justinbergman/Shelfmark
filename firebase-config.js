29320f007027f4c00476258d0a21192ebcd4cc60
justinbergman <127881965+justinbergman@users.noreply.github.com>
2026-09-11 11:18:42 -0500

    Update Firebase initialization to use firebase namespace
    Update to the file based on Claude fixing some errors

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,4 +12,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
